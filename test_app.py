c99f7984d83f84ca0e6d2407d0841c66ab27ac3e
Basmatamer-21 <basmatamer2004@gmail.com>
2025-07-19 12:44:33 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
-○ from app import app
-○ import unittest
-○ class TestApp(unittest.TestCase):
-○ def test_home(self):
-○ tester = app.test_client(self)
-○ response = tester.get('/')
-○ self.assertEqual(response.status_code, 200)
+from app import app
+import unittest
+class TestApp(unittest.TestCase):
+ def test_home(self):
+  tester = app.test_client(self)
+  response = tester.get('/')
+  self.assertEqual(response.status_code, 200)
