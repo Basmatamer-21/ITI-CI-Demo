299dfd5c6c91b56d644fe20fe6b7778c13e7d68e
Basmatamer-21 <basmatamer2004@gmail.com>
2025-07-19 17:11:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD!"
+    return "Hello, ITI!"
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run()
